163682fc0e0a61c8fa74fbf580f424900471a8db
San <yan_san_din@mail.ru>
2023-02-07 09:27:14 +0300

    41

---
+++ 41/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] Fillarray()
+{
+    int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    
+    
+    return array;
+}
+
+int SummElements(int [] array)
+{
+    int count=0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]<0)
+        count++;
+    
+    }
+    return count;
+}
+
+void PrintArray(int [] array)
+{
+    Console.Write($"[{String.Join(",",array)}]\n");
+    
+}
+
+Console.Write("Введите числа: ");
+int[] m = Fillarray();
+
+
+PrintArray(m);
+
+int[] array= m;
+int count= SummElements(array);
+Console.WriteLine($"Количество отрицательных чисел: {count}");
